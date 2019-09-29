548a9ade7d5155edab42d261d5d99a5349d2e49a
Imran Hossain <imran.dev.bd@gmail.com>
2019-09-30 03:10:17 +0600

    Update

---
+++ database/migrations/2019_09_29_134554_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('sub_title')->nullable();
+            $table->string('slug');
+            $table->string('category_ids')->nullable();
+            $table->decimal('price');
+            $table->decimal('sale_price');
+            $table->string('product_photo')->nullable();
+            $table->text('description')->nullable();
+            $table->text('weight', 50)->nullable();
+            $table->integer('stock');
+            $table->tinyInteger('featured');
+            $table->enum('status', ['Publish', 'Draft', 'Inactive'])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
